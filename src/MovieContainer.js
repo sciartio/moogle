f9265e2c12b7bb61d302c7a1d5d2d21fcbe4452b
sciart <sciart@gmail.com>
2022-04-29 19:34:55 -0500

    cleaned up

--- src/MovieContainer.js
+++ src/MovieContainer.js
@@ -1,13 +1,7 @@
 import MovieCard from './MovieCard.js';
-import { useEffect } from 'react';
 
 const MovieContainer = ( {movies} ) => {
 
-    useEffect( () => {
-        console.log("BABO");
-        console.log(movies);
-    }, []);
-    
     return (
 
          <section className="movie-container">
